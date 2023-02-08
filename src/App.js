bdbaf45d2963567b613b0db9e0feddd66b8fdf87
majedulleon <majedulleon@gmail.com>
2023-02-08 13:57:30 +0600

    Basic Features and Syntex

--- src/App.js
+++ src/App.js
@@ -12,15 +12,45 @@ class App extends Component {
       { bookName: "Amphibian Man", writter: "Alexander Belaiev" }
     ]
   }
+
+  changeBookState = () => {
+    this.setState(
+      {
+        book: [
+          { bookName: "1984", writter: "Goerge Orwell" },
+          { bookName: "The Da Vinci Code", writter: "Dan Brown" },
+          { bookName: "Treasure Island", writter: "Robert Luis Stevenson" },
+          { bookName: "Farenhite 451", writter: "Goerge Orwell" },
+          { bookName: "Amphibian Man", writter: "Alexander Belaiev" }
+        ]
+      }
+    )
+  }
+
+  changeWithInput = (event) => {
+    this.setState(
+      {
+        book: [
+          { bookName: event.target.value, writter: "Goerge Orwell" },
+          { bookName: "The Da Vinci Code", writter: "Dan Brown" },
+          { bookName: "Treasure Island", writter: "Robert Luis Stevenson" },
+          { bookName: "Farenhite 451", writter: "Goerge Orwell" }
+        ]
+      }
+    )
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className='App'>
-        <h1>Book List :-</h1>
-        <Book bookName={this.state.book[0].bookName} writter={this.state.book[0].writter} />
+        <h1 className='title'>Book List :-</h1>
+        <button onClick={this.changeBookState}>Change StatE</button> <br />
+        <input type="text" onChange={this.changeWithInput}></input>
+        <Book bookName={this.state.book[0].bookName} writter={this.state.book[0].writter} inputName={this.changeWithInput} />
         <Book bookName={this.state.book[1].bookName} writter={this.state.book[1].writter} />
         <Book bookName={this.state.book[2].bookName} writter={this.state.book[2].writter} />
         <Book bookName={this.state.book[3].bookName} writter={this.state.book[3].writter} />
-        <Book bookName={this.state.book[4].bookName} writter={this.state.book[4].writter} />
       </div>
     );
   }
--- src/Components/Book.js
+++ src/Components/Book.js
@@ -5,6 +5,8 @@ let Book = (props) => {
         <div>
             <h3>Book Name: {props.bookName}</h3>
             <h4>Author: {props.writter}</h4>
+            <input type="text" onChange={props.inputName} value={props.bookName} />
+            <p></p>
         </div>
     );
 }
